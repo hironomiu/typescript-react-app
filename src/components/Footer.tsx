74352f9dae10fb737efe7505c1c7d4dd574da150
hironomiu <hironomiu@gmail.com>
2022-07-26 12:09:32 +0900

    commited

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <footer className="max-w-[1240px] py-16 px-4 mx-auto grid md:grid-cols-3 gap-8">
+      <div>
+        <h1 className="w-full text-3xl text-[#00df9a]">Web Site</h1>
+        <p className="py-4">OK! hey! are are are OK!OK! bye!thank you!</p>
+        <div className="flex justify-between md:w-[75%] mt-6">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
+        </div>
+      </div>
+      <div>fuga</div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Header = () => {
+  const [isSideNav, setIsSideNav] = useState(false)
+  const handleClickIsNav = () => {
+    setIsSideNav(!isSideNav)
+  }
+  return (
+    <header className="flex justify-between items-center h-24 mx-auto px-4">
+      <h1 className="w-full text-3xl text-[#00df9a]">Web Site</h1>
+      <nav className="hidden md:flex">
+        <ul className="flex ">
+          <li className="px-2 text-xl">Hoge</li>
+          <li className="px-2 text-xl">Fuga</li>
+          <li className="px-2 text-xl">Piyo</li>
+          <li className="px-2 text-xl">Gogo</li>
+        </ul>
+      </nav>
+      <div className="block md:hidden" onClick={handleClickIsNav}>
+        {!isSideNav ? (
+          <AiOutlineMenu size={20} />
+        ) : (
+          <AiOutlineClose size={20} />
+        )}
+      </div>
+      <div
+        className={
+          isSideNav
+            ? 'md:hidden fixed top-0 left-0 h-full w-[60%] bg-gray-900 ease-in-out duration-500 pl-4'
+            : 'fixed left-[-100%] top-0 h-full ease-in-out duration-1000'
+        }
+      >
+        <h1 className="w-full text-3xl mt-8">Side Menu　　　　　</h1>
+        <ul className="flex flex-col pt-10">
+          <li className="px-2 text-xl border-b border-gray-300 pt-4 pb-2 mr-4">
+            Hoge
+          </li>
+          <li className="px-2 text-xl border-b border-gray-300 pt-4 pb-2 mr-4">
+            Fuga
+          </li>
+          <li className="px-2 text-xl border-b border-gray-300 pt-4 pb-2 mr-4">
+            Piyo
+          </li>
+          <li className="px-2 text-xl border-b border-gray-300 pt-4 pb-2 mr-4">
+            Gogo
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+
+export default Header
