5cbe04060f022757229a89b2f48ed41c57a68dca
hironomiu <hironomiu@gmail.com>
2022-07-29 22:11:58 +0900

    commited

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,14 @@
 import Top from './main/Top'
 import Second from './main/Second'
 import Third from './main/Third'
+import Fourth from './main/Fourth'
 const Main = () => {
   return (
     <main className="">
       <Top />
       <Second />
       <Third />
+      <Fourth />
     </main>
   )
 }
---
+++ src/components/main/Fourth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Fourth = () => {
+  return (
+    <div id="fourth" className="bg-white text-black w-full h-[100vh]">
+      <div className="grid md:grid-cols-3 mx-auto gap-8 py-16 px-4">
+        <div>hoge</div>
+        <div>hoge</div>
+        <div>hoge</div>
+      </div>
+    </div>
+  )
+}
+
+export default Fourth
